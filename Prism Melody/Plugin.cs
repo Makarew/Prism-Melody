7e93ad1e511a2d634870f4e0695f32adf8bff745
Nullctipus <None>
2023-05-17 13:44:23 -0500

    Use Game's Singleton System

--- Prism Melody/Plugin.cs
+++ Prism Melody/Plugin.cs
@@ -17,9 +17,7 @@ namespace Prism_Melody
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            base.OnSceneWasLoaded(buildIndex, sceneName);
-
-            if (GameObject.FindObjectOfType<GameManager>() && sceneName == "CharacterSelect")
+            if (GlobalManager.Instance && GlobalManager.Instance.GameManager && sceneName == "CharacterSelect")
             {
                 LoadPalettes();
             }
@@ -27,44 +25,11 @@ namespace Prism_Melody
 
         internal void LoadPalettes()
         {
-            Idol[] idols = GameObject.FindObjectOfType<GameManager>().Characters;
-
-            for (int i = 0; i < idols.Length; i++)
+            foreach(Idol idol in GlobalManager.Instance.GameManager.Characters)
             {
-                string charName = "";
-                switch (i)
-                {
-                    case 0:
-                        charName = "Aki";
-                        break;
-                    case 1:
-                        charName = "Ayame";
-                        break;
-                    case 2:
-                        charName = "Coco";
-                        break;
-                    case 3:
-                        charName = "Fubuki";
-                        break;
-                    case 4:
-                        charName = "Korone";
-                        break;
-                    case 5:
-                        charName = "Sora";
-                        break;
-                    case 6:
-                        charName = "Suisei";
-                        break;
-                    case 7:
-                        charName = "Botan";
-                        break;
-                }
-
-                Idol idol = idols[i];
-
                 for (int j = 0; j < idol.paletteSwapMaterials.Count; j++)
                 {
-                    string filePath = Path.Combine(MelonHandler.PluginsDirectory, "Palettes", charName + j.ToString());
+                    string filePath = Path.Combine(MelonHandler.PluginsDirectory, "Palettes", idol.charName + j);
 
                     List<Vector4> inColors = new List<Vector4>();
                     List<Vector4> outColors = new List<Vector4>();
@@ -73,59 +38,49 @@ namespace Prism_Melody
 
                     if (File.Exists(filePath + "in.txt"))
                     {
-                        MelonLogger.Msg("Found an In Palette For Outfit " + j + " For " + charName);
-
-                        StreamReader sr = new StreamReader(filePath + "in.txt");
+                        MelonLogger.Msg("Found an In Palette For Outfit " + j + " For " + idol.charName);
 
                         string line;
-                        using (sr)
+                        int k = 0;
+                        using (StreamReader sr = new StreamReader(filePath + "in.txt"))
                         {
                             do
                             {
                                 line = sr.ReadLine();
-                                if (line != null)
+                                if (!string.IsNullOrEmpty(line))
                                 {
                                     string[] lineData = line.Split(';');
+                                    if(lineData.Length<4) continue; // Check for bad lines
 
-                                    inColors.Add(new Vector4(float.Parse(lineData[0]), float.Parse(lineData[1]), float.Parse(lineData[2]), float.Parse(lineData[3])));
+                                    mat.SetVector("inColour" + k++, new Vector4(float.Parse(lineData[0]), float.Parse(lineData[1]), float.Parse(lineData[2]), float.Parse(lineData[3])));
                                 }
-                            } while (line != null);
+                            } while (!string.IsNullOrEmpty(line));
 
                             sr.Close();
-                        }
-
-                        for (int k = 0; k < inColors.Count; k++)
-                        {
-                            mat.SetVector("inColour" + k, inColors[k]);
                         }
                     }
 
                     if (File.Exists(filePath + "out.txt"))
                     {
-                        MelonLogger.Msg("Found an Out Palette For Outfit " + j + " For " + charName);
-
-                        StreamReader sr = new StreamReader(filePath + "out.txt");
+                        MelonLogger.Msg("Found an Out Palette For Outfit " + j + " For " + idol.charName);
 
                         string line;
-                        using (sr)
+                        int k = 0;
+                        using (StreamReader sr = new StreamReader(filePath + "out.txt"))
                         {
                             do
                             {
                                 line = sr.ReadLine();
-                                if (line != null)
+                                if (!string.IsNullOrEmpty(line))
                                 {
                                     string[] lineData = line.Split(';');
+                                    if(lineData.Length<4) continue; // Check for bad lines
 
-                                    outColors.Add(new Vector4(float.Parse(lineData[0]), float.Parse(lineData[1]), float.Parse(lineData[2]), float.Parse(lineData[3])));
+                                    mat.SetVector("outColour" + k++, new Vector4(float.Parse(lineData[0]), float.Parse(lineData[1]), float.Parse(lineData[2]), float.Parse(lineData[3])));
                                 }
-                            } while (line != null);
+                            } while (!string.IsNullOrEmpty(line));
 
                             sr.Close();
-                        }
-
-                        for (int k = 0; k < outColors.Count; k++)
-                        {
-                            mat.SetVector("outColour" + k, outColors[k]);
                         }
                     }
                 }
